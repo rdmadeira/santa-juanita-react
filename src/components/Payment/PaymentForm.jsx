2f3349ad508bde20cec9b8abee3f150595205945
rdmadeira <rdmadeira2@gmail.com>
2023-01-22 20:48:00 -0300

    input creditcardnumber validator, falta resolver

--- src/components/Payment/PaymentForm.jsx
+++ src/components/Payment/PaymentForm.jsx
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 import SignInUpInput from '../sign-in-up/SignInUpInput.jsx';
 import {
   VALIDATOR_TYPE_REQUIRE,
-  VALIDATE_LENGTH,
+  VALIDATE_CREDIT_CARD,
 } from '../../utils/form_utils/validateInputs';
+import useForm from '../../hooks/useForm';
 
 const StyledForm = styled.form`
   display: flex;
@@ -12,13 +13,39 @@ const StyledForm = styled.form`
 `;
 
 export const PaymentForm = () => {
+  const initialInputs = {
+    cardNumber: {
+      value: '',
+      isValid: false,
+    },
+    expiryDate: {
+      value: '',
+      isValid: false,
+    },
+    cardName: {
+      value: '',
+      isValid: false,
+    },
+  };
+  const [formState, inputHandle, setFormData] = useForm(initialInputs, false);
   return (
     <StyledForm>
       <h2>Agregue Medio de Pago</h2>
       <SignInUpInput
         type="text"
+        id="creditCardNumber"
         validators={[VALIDATOR_TYPE_REQUIRE()]}
         name="cardName"
+        onInput={inputHandle}
+        errorText="Insert a Valid Credit Card Number"
+      />
+      <SignInUpInput
+        type="text"
+        id="creditCardNumber"
+        validators={[VALIDATOR_TYPE_REQUIRE(), VALIDATE_CREDIT_CARD()]}
+        name="cardName"
+        onInput={inputHandle}
+        errorText="Insert a Valid Credit Card Number"
       />
     </StyledForm>
   );
