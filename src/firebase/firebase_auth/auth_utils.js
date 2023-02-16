710015a77b6367801e21824fbdf860a0f6f8e27b
rdmadeira <rdmadeira2@gmail.com>
2023-02-15 22:09:01 -0300

    trying signinwithlink to work

--- src/firebase/firebase_auth/auth_utils.js
+++ src/firebase/firebase_auth/auth_utils.js
@@ -26,19 +26,18 @@ const auth = getAuth(app);
 const actionCodeSettings = {
   // URL you want to redirect back to. The domain (www.example.com) for this
   // URL must be in the authorized domains list in the Firebase Console.
-  url: 'https://santa-juanita-react.vercel.app/',
+  url: 'https://santajuanita.page.link/V9Hh',
   // This must be true.
   handleCodeInApp: true,
 };
 
-export const signUpWithEmail = (infoemail) => {
-  sendSignInLinkToEmail(auth, infoemail, actionCodeSettings)
+export const signUpWithEmail = (email) => {
+  sendSignInLinkToEmail(auth, email, actionCodeSettings)
     .then(() => {
       // The link was successfully sent. Inform the user.
       // Save the email locally so you don't need to ask the user for it again
       // if they open the link on the same device.
-      alert('The link was successfully sent');
-      window.localStorage.setItem('emailForSignIn', infoemail);
+      window.localStorage.setItem('emailForSignIn', email);
       // ...
     })
     .catch((error) => {
@@ -63,20 +62,17 @@ export const signUpWithEmail = (infoemail) => {
       .then((result) => {
         // Clear email from storage.
         window.localStorage.removeItem('emailForSignIn');
-        console.log(result.user, result.providerId);
-
-        return { data: result.user, id: result.providerId };
-
         // You can access the new user via result.user
+        console.log(result.user, result.providerId);
         // Additional user info profile not available via:
         // result.additionalUserInfo.profile == null
         // You can check if the user is new or existing:
         // result.additionalUserInfo.isNewUser
       })
       .catch((error) => {
+        console.log(error);
         // Some error occurred, you can inspect the code: error.code
         // Common errors could be invalid email and invalid or expired OTPs.
-        console.log(error);
       });
   }
 };
